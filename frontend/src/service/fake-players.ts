094ab614f89ad3ef606ed2d4bb5508eb9da53ffc
Tsungai Chipato <tsungai@kryha.io>
2022-08-24 13:49:26 +0200

    update fake data

--- frontend/src/service/fake-players.ts
+++ frontend/src/service/fake-players.ts
@@ -11,31 +11,37 @@ import {
 
 export const Players: Player[] = [
   {
+    id: "1",
     name: "umpalumpa",
     color: "#FFC300",
     avatar: ToyHand,
   },
   {
+    id: "2",
     name: "señor.garfio",
     color: "#FF8059",
     avatar: HookHand,
   },
   {
+    id: "3",
     name: "shake.it",
     color: "#FFA7E9",
     avatar: PlasticHand,
   },
   {
+    id: "4",
     name: "something.great",
     color: "#989EFF",
     avatar: ScooperHand,
   },
   {
+    id: "5",
     name: "god",
     color: "#92C9FF",
     avatar: Hand,
   },
   {
+    id: "6",
     name: "lekker",
     color: "#91C342",
     avatar: LobsterHand,
