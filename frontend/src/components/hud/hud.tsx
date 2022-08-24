1da0b94489f8e77f907678f57aed4b7b260c89f4
Tsungai Chipato <tsungai@kryha.io>
2022-08-24 15:32:35 +0200

    remove power up

--- frontend/src/components/hud/hud.tsx
+++ frontend/src/components/hud/hud.tsx
@@ -1,17 +1,11 @@
 import { FC } from "react";
 import { Dice } from "../../interfaces/hud";
 import { DiceOverview } from "../dice-overview";
-import { PowerUpOverview } from "../power-up-overview";
 
 interface HudProps {
   dice: Dice[];
 }
 
 export const Hud: FC<HudProps> = ({ dice }) => {
-  return (
-    <>
-      <DiceOverview dice={dice} />
-      <PowerUpOverview />
-    </>
-  );
+  return <DiceOverview dice={dice} />;
 };
--- frontend/src/components/power-up/power-up.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-
-import { PowerUpWrapper } from "./styles";
-
-// TODO: update component when designs are updated
-export const PowerUp: FC = () => {
-  return <PowerUpWrapper />;
-};
