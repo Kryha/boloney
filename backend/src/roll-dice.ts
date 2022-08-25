26ba86af8ca339c5224d5351b9045805b30c8855
Mauro Eijsenring <mauro@kryha.io>
2022-08-25 07:57:02 +0000

    Merged PR 2558: First implementation of the rolling dice service on the game server.
    Created:
    - a function that calls an external API
    - a custom RPC call for rolling a dice that handles the response of the external call.
    
    I am not sure about the return types that I created. because I am not sure how they will be used in the fronted.
    
    Related work items: #11402

--- backend/src/roll-dice.ts
+++ backend/src/roll-dice.ts
@@ -1,4 +1,53 @@
-function rpcRollDice(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string): string {
-  logger.info("Dice is rolled rpc is called");
-  return JSON.stringify({ throw: Math.floor(Math.random() * 6) + 1, success: true });
+interface DiceThrowPayload {
+  diceAmount: number;
 }
+interface DiceThrowResponse {
+  rollResult: number[];
+}
+
+// TODO: Update this url to our own
+const apiBaseUrl: string = "http://www.randomnumberapi.com/api/v1.0";
+
+const getDiceRoll: nkruntime.RpcFunction = (_ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payload: string) => {
+  // The code below is to check if the clients send a correct payload for the server
+  let json: DiceThrowPayload;
+  try {
+    json = JSON.parse(payload);
+  } catch (error) {
+    logger.error("An error occured parsing the payload: %s", getErrorMessage(error));
+    return;
+  }
+
+  //Calling the external server and formatting the results
+  let response: DiceThrowResponse;
+
+  try {
+    response = throwDice(nk, json);
+  } catch (error) {
+    logger.error("An error occured throwing the dice: %s", getErrorMessage(error));
+    return;
+  }
+
+  return JSON.stringify(response);
+};
+
+//This function handles the external call
+const throwDice = (nk: nkruntime.Nakama, payload: DiceThrowPayload): DiceThrowResponse => {
+  const url = apiBaseUrl + "/random";
+  const headers = { "Content-Type": "application/json" };
+  const body = {
+    min: 1,
+    max: 6,
+    count: payload.diceAmount,
+  };
+  const res = nk.httpRequest(url, "get", headers, JSON.stringify(body));
+
+  return {
+    rollResult: JSON.parse(res.body),
+  };
+};
+
+const getErrorMessage = (error: unknown) => {
+  if (error instanceof Error) return error.message;
+  return String(error);
+};
