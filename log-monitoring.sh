950aa8837bd529de6b324370b7e6f47f7795810e
Nick Koster <nick@kryha.io>
2022-09-22 21:24:57 +0200

    added shell script to make log monitoring easier

---
+++ log-monitoring.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+NAMESPACE="liars-dice-local"  # could be passed as an argument in the future
+PODLIST=`kubectl -n ${NAMESPACE} get pods`
+
+while getopts 'fb' OPTION; do
+  case "$OPTION" in
+    f)
+      NAME=${PODLIST##*frontend-}
+      NAME=${NAME::16}
+      NAME="frontend-${NAME}"
+      ;;
+    b)
+      NAME=${PODLIST##*backend-}
+      NAME=${NAME::16}
+      NAME="backend-${NAME}"
+      ;;
+  esac
+done
+
+if [ -z "$NAME" ]
+then
+    echo "Script usage: [-f] [-b] for either the frontend or backend log monitoring"
+else
+    kubectl -n ${NAMESPACE} logs $NAME -f
+fi
