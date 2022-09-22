352d7092ac25d34e38417f77ee828b8441f06cc4
Tsungai Chipato <tsungai@kryha.io>
2022-09-22 09:54:47 +0200

    wip

--- frontend/src/components/new-game-hands/new-game-hands.tsx
+++ frontend/src/components/new-game-hands/new-game-hands.tsx
@@ -1,19 +1,3 @@
-import {
-  ToyHand,
-  ToyHandPaint,
-  PlasticHand,
-  PlasticHandPaint,
-  HookHand,
-  HookHandPaint,
-  ScooperHand,
-  ScooperHandPaint,
-  HandPaint,
-  Hand as HandImg,
-  SkeletonHand,
-  LobsterHand,
-  LobsterHandPaint,
-  SkeletonHandPaint,
-} from "../../assets";
 import { useViewport } from "../../hooks/use-viewport";
 import { Hand } from "../hand";
 import { NewGameHandsWrapper } from "./styles";
@@ -23,13 +7,13 @@ export const NewGameHands = () => {
 
   return (
     <NewGameHandsWrapper width={width} height={height}>
-      <Hand avatar={PlasticHand} paint={PlasticHandPaint} avatarName="plastic" />
-      <Hand avatar={HookHand} paint={HookHandPaint} avatarName="hook" />
-      <Hand avatar={ScooperHand} paint={ScooperHandPaint} avatarName="scooper" />
-      <Hand avatar={HandImg} paint={HandPaint} avatarName="hand" />
-      <Hand avatar={SkeletonHand} paint={SkeletonHandPaint} avatarName="skeleton" />
-      <Hand avatar={ToyHand} paint={ToyHandPaint} avatarName="toy" />
-      <Hand avatar={LobsterHand} paint={LobsterHandPaint} avatarName="lobster" />
+      <Hand avatarName="plastic" />
+      <Hand avatarName="hook" />
+      <Hand avatarName="scooper" />
+      <Hand avatarName="hand" />
+      <Hand avatarName="skeleton" />
+      <Hand avatarName="toy" />
+      <Hand avatarName="lobster" />
     </NewGameHandsWrapper>
   );
 };
