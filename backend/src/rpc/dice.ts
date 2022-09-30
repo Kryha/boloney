05add30ab3c03988a6d6314a6d7d93ac37e3d0dd
Marius Sili <sili.marius.me@gmail.com>
2022-09-30 14:32:11 +0200

    Removed hardcoded values

--- backend/src/rpc/dice.ts
+++ backend/src/rpc/dice.ts
@@ -1,17 +1,18 @@
 import { z } from "zod";
 
-import { range, rpcHandler } from "../utils";
+import { MAX_DICE_PER_PLAYER, MIN_DICE_PER_PLAYER, range, rpcHandler } from "../utils";
 
 export const rollSchema = z.object({
-  diceAmount: z.number().min(1).max(5),
+  diceAmount: z.number().min(MIN_DICE_PER_PLAYER).max(MAX_DICE_PER_PLAYER),
 });
 
 export const rollDice = rpcHandler((_ctx, _logger, _nk, payload) => {
   // TODO: implement wanted behaviour for this function
-  // 1. check if user is allowed to roll
-  // 2. call toolkit
-  // 3. store generated keys
-  // 4. update game state
+  // - check if user is allowed to roll
+  // - check if diceAmount respects the game settings
+  // - call toolkit
+  // - store generated proofs
+  // - update game state
 
   const { diceAmount } = rollSchema.parse(JSON.parse(payload));
 
