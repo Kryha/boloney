c945f4c32c91a1a86736b664c63cfddaf210d961
Marius Sili <sili.marius.me@gmail.com>
2022-10-04 11:42:34 +0200

    Added todo

--- backend/src/utils/const.ts
+++ backend/src/utils/const.ts
@@ -23,6 +23,7 @@ export const enum OP {
   Reject = 4,
 }
 
+// TODO: configure env vars and set to env var if present
 export const TOOLKIT_BASE_URL = "http://zk-gaming-tk.zk-gaming-tk-local.svc.cluster.local:5001";
 
 export const MAX_DICE_PER_PLAYER = 10;
