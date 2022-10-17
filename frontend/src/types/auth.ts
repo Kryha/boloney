91b2aa8799b437b69f3b6511e05844d88d178a66
Marius Sili <sili.marius.me@gmail.com>
2022-10-17 17:05:02 +0200

    Checking for keys and account length and prefix on frontend

--- frontend/src/types/auth.ts
+++ frontend/src/types/auth.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
+import { ADDRESS_LENGTH, ADDRESS_PREFIX, PRIVATE_KEY_LENGTH, PRIVATE_KEY_PREFIX, VIEW_KEY_LENGTH, VIEW_KEY_PREFIX } from "../constants";
 
+// TODO: check for length and prefix
 export const accountKeysSchema = z.object({
-  privateKey: z.string(),
-  viewKey: z.string(),
-  address: z.string(),
+  privateKey: z.string().length(PRIVATE_KEY_LENGTH).startsWith(PRIVATE_KEY_PREFIX),
+  viewKey: z.string().length(VIEW_KEY_LENGTH).startsWith(VIEW_KEY_PREFIX),
+  address: z.string().length(ADDRESS_LENGTH).startsWith(ADDRESS_PREFIX),
 });
 
 export type AccountKeys = z.infer<typeof accountKeysSchema>;
