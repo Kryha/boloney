6890b0ad13da985194ffb7d0786060367c2198f5
Tsungai Chipato <tsungai@kryha.io>
2022-10-18 10:24:52 +0200

    pr fix

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -33,6 +33,7 @@ export const FLOATING_ANIMATION_SPEED = 4;
 export const AUTH_TOKEN_STORAGE_KEY = "auth_token";
 export const REFRESH_TOKEN_STORAGE_KEY = "refresh_token";
 
+// TODO: define these in a separate file
 export const RPC_CREATE_MATCH = "create_match";
 export const RPC_FIND_MATCH = "find_match";
 
