e5a89d005de96677aeb8df50432913cf9f788d2c
Tsungai Chipato <tsungai@kryha.io>
2022-10-19 09:54:24 +0200

    add todo

--- frontend/src/types/power-up.ts
+++ frontend/src/types/power-up.ts
@@ -2,4 +2,13 @@ import { z } from "zod";
 
 export const powerUpTypeSchema = z.enum(["p1", "p2", "p3", "p4"]);
 
-export type PowerupType = z.infer<typeof powerUpTypeSchema>;
+export type PowerUpType = z.infer<typeof powerUpTypeSchema>;
+
+// TODO: update type and update type on the backend
+export const powerUpSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  image: z.string(),
+});
+
+export type PowerUp = z.infer<typeof powerUpSchema>;
