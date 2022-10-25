dcb3d9922db025938ff635b4bf6e07a9cf21b72c
Tsungai Chipato <tsungai@kryha.io>
2022-10-25 13:59:46 +0200

    add useefect

--- frontend/src/service/match.ts
+++ frontend/src/service/match.ts
@@ -1,5 +1,5 @@
 import { MatchData } from "@heroiclabs/nakama-js";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { text } from "../assets";
 import { useAuthState, useMatchMakerState } from "../store";
 import { useMatchState } from "../store/match";
@@ -20,33 +20,35 @@ export const useMatch = () => {
 
   if (!socket) throw new Error(text.error.noSocketConnected);
 
-  socket.onmatchdata = (matchData: MatchData) => {
-    if (matchData.op_code === MatchOpCode.STAGE_TRANSITION) {
-      // setRoundStage(matchData.data.matchStage);
-      setRoundStage(RoundStage.ROLL_DICE_STAGE);
-      // add a switch statement and then save data based on stage
-      // TODO: use matchData.data.matchStage
-      switch (roundStage) {
-        case RoundStage.GET_POWERUP_STAGE:
-          setPowerUps(fakePowerUps);
-          break;
-        case RoundStage.ROLL_DICE_STAGE:
-          setFaceValues(fakeDiceRolls);
-          break;
-        case RoundStage.PLAYER_TURN_STAGE:
-          // TODO: add other phases
-          break;
-        case RoundStage.ROUND_SUMMARY_STAGE:
-          break;
-        case RoundStage.END_OF_MATCH_STAGE:
-          break;
-        default:
-          // add an error
-          setPowerUps(fakePowerUps);
-          break;
+  useEffect(() => {
+    socket.onmatchdata = (matchData: MatchData) => {
+      if (matchData.op_code === MatchOpCode.STAGE_TRANSITION) {
+        // setRoundStage(matchData.data.matchStage);
+        setRoundStage(RoundStage.ROLL_DICE_STAGE);
+        // add a switch statement and then save data based on stage
+        // TODO: use matchData.data.matchStage
+        switch (roundStage) {
+          case RoundStage.GET_POWERUP_STAGE:
+            setPowerUps(fakePowerUps);
+            break;
+          case RoundStage.ROLL_DICE_STAGE:
+            setFaceValues(fakeDiceRolls);
+            break;
+          case RoundStage.PLAYER_TURN_STAGE:
+            // TODO: add other phases
+            break;
+          case RoundStage.ROUND_SUMMARY_STAGE:
+            break;
+          case RoundStage.END_OF_MATCH_STAGE:
+            break;
+          default:
+            // add an error
+            setPowerUps(fakePowerUps);
+            break;
+        }
       }
-    }
-  };
+    };
+  }, [roundStage, setFaceValues, setPowerUps, setRoundStage, socket]);
 
   const sendMatchState = useCallback(
     async (payload: string): Promise<NkResponse> => {
