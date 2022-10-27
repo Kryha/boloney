32b3bf5aedcf338fb85644efb48ce05b409e92c6
Mauro Eijsenring <mauro@kryha.io>
2022-10-27 18:53:47 +0200

    Avatar refactor frontend

--- frontend/src/components/game-player/game-player.tsx
+++ frontend/src/components/game-player/game-player.tsx
@@ -4,6 +4,7 @@ import { avatarHeight } from "../atoms";
 import { GamePlayersWrapper, PlayerColor, PlayerNameContainer, PlayerAvatar, PlayerName as Name } from "./styles";
 import { handProportion } from "../../design/hand";
 import { Player } from "../../types";
+import { avatars } from "../../assets";
 
 interface GamePlayerProps {
   totalPlayers: number;
@@ -24,11 +25,11 @@ export const PlayerName: FC<PlayerNameProps> = ({ name, color }) => {
 };
 
 export const GamePlayer: FC<GamePlayerProps> = ({ totalPlayers, player }) => {
-  const { avatar } = handProportion(player.avatarName);
+  const { avatar } = handProportion(avatars[player.avatarId].name);
   return (
     <GamePlayersWrapper>
       <PlayerAvatar src={avatar} alt={player.username} height={avatarHeight[totalPlayers - 1]} />
-      <PlayerName name={player.username} color={player.color} />
+      <PlayerName name={player.username} color={avatars[player.avatarId].color} />
     </GamePlayersWrapper>
   );
 };
