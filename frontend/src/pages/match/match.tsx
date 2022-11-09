99c203270ab460130b267665085c59ca8df82a41
Tsungai Chipato <tsungai@kryha.io>
2022-11-09 10:40:57 +0100

    wip

--- frontend/src/pages/match/match.tsx
+++ frontend/src/pages/match/match.tsx
@@ -15,14 +15,15 @@ import {
   Lobby,
 } from "../../components";
 import { routes } from "../../navigation";
-import { useMatch, useMatchMaker } from "../../service";
+import { fakeDiceRolls, fakeLocalPlayer, fakePlayers, useMatch, useMatchMaker } from "../../service";
+import { fakePowerUps } from "../../service/fake-power-ups";
 import { useStore } from "../../store";
 import { isPlayerOrderObject, isPlayerRecord, isStageTransition, MatchOpCode, MatchStage } from "../../types";
 import { parseMatchData, parseMatchIdParam } from "../../util";
 
 export const Match = () => {
   const { joinMatch } = useMatchMaker();
-  const { matchStage, isLoading, getOrderedPlayers, handleStageTransition, getLocalPlayer } = useMatch();
+  const { matchStage, isLoading } = useMatch();
   const powerUps = useStore((state) => state.powerUps);
   const faceValues = useStore((state) => state.faceValues);
   const players = useStore((state) => state.players);
@@ -33,9 +34,9 @@ export const Match = () => {
   const setPlayers = useStore((state) => state.setPlayers);
   const setPlayerOrder = useStore((state) => state.setPlayerOrder);
 
-  // TODO: Check if we need to re-stablish socket conection after reloading the page
-  const { matchId: unparsedId } = useParams();
-  const matchId = parseMatchIdParam(unparsedId);
+  // // TODO: Check if we need to re-stablish socket conection after reloading the page
+  // const { matchId: unparsedId } = useParams();
+  // const matchId = parseMatchIdParam(unparsedId);
 
   const getStageComponent = (stage: MatchStage): ReactNode => {
     switch (stage) {
@@ -52,62 +53,57 @@ export const Match = () => {
     }
   };
 
-  useEffect(() => {
-    if (matchId && session?.username) joinMatch(matchId, { username: session.username });
-  }, [joinMatch, matchId, session?.username]);
+  // useEffect(() => {
+  //   if (matchId && session?.username) joinMatch(matchId, { username: session.username });
+  // }, [joinMatch, matchId, session?.username]);
 
-  useEffect(() => {
-    if (!socket) return;
+  // useEffect(() => {
+  //   if (!socket) return;
 
-    socket.onmatchdata = (matchData: MatchData) => {
-      const matchOpcode = matchData.op_code;
+  //   socket.onmatchdata = (matchData: MatchData) => {
+  //     const matchOpcode = matchData.op_code;
 
-      // TODO: Add cases for the rest of the OP_CODES
-      switch (matchOpcode) {
-        case MatchOpCode.STAGE_TRANSITION: {
-          const data = parseMatchData(matchData.data);
-          const stage: MatchStage = data.matchStage;
-          if (!isStageTransition(data)) return;
-          setMatchStage(stage);
-          handleStageTransition(stage);
-          break;
-        }
-        case MatchOpCode.PLAYER_JOINED: {
-          const players = parseMatchData(matchData.data);
-          if (!isPlayerRecord(players)) return;
-          setPlayers(players);
-          break;
-        }
-        case MatchOpCode.PLAYER_READY: {
-          const players = parseMatchData(matchData.data);
-          if (!isPlayerRecord(players)) return;
-          setPlayers(players);
-          break;
-        }
-        case MatchOpCode.PLAYER_ORDER_SHUFFLE: {
-          const playerOrder = parseMatchData(matchData.data);
-          if (!isPlayerOrderObject(playerOrder)) return;
-          setPlayerOrder(playerOrder.playerOrder);
-          break;
-        }
-      }
-    };
-  }, [socket, setMatchStage, setPlayerOrder, setPlayers, handleStageTransition]);
+  //     // TODO: Add cases for the rest of the OP_CODES
+  //     switch (matchOpcode) {
+  //       case MatchOpCode.STAGE_TRANSITION: {
+  //         const data = parseMatchData(matchData.data);
+  //         const stage: MatchStage = data.matchStage;
+  //         if (!isStageTransition(data)) return;
+  //         setMatchStage(stage);
+  //         handleStageTransition(stage);
+  //         break;
+  //       }
+  //       case MatchOpCode.PLAYER_JOINED: {
+  //         const players = parseMatchData(matchData.data);
+  //         if (!isPlayerRecord(players)) return;
+  //         setPlayers(players);
+  //         break;
+  //       }
+  //       case MatchOpCode.PLAYER_READY: {
+  //         const players = parseMatchData(matchData.data);
+  //         if (!isPlayerRecord(players)) return;
+  //         setPlayers(players);
+  //         break;
+  //       }
+  //       case MatchOpCode.PLAYER_ORDER_SHUFFLE: {
+  //         const playerOrder = parseMatchData(matchData.data);
+  //         if (!isPlayerOrderObject(playerOrder)) return;
+  //         setPlayerOrder(playerOrder.playerOrder);
+  //         break;
+  //       }
+  //     }
+  //   };
+  // }, [socket, setMatchStage, setPlayerOrder, setPlayers, handleStageTransition]);
 
-  if (!matchId || !session?.user_id) return <Navigate to={routes.home} />;
+  // if (!matchId || !session?.user_id) return <Navigate to={routes.home} />;
 
-  // TODO: add loading animation
-  if (isLoading) return <Heading2>{text.general.loading}</Heading2>;
+  // // TODO: add loading animation
+  // if (isLoading) return <Heading2>{text.general.loading}</Heading2>;
 
-  if (matchStage === "lobbyStage") return <Lobby />;
+  // if (matchStage === "lobbyStage") return <Lobby />;
 
   return (
-    <GameLayout
-      players={getOrderedPlayers(players, playersOrder)}
-      dice={faceValues}
-      powerUps={powerUps}
-      localPlayer={getLocalPlayer(players, session.user_id)}
-    >
+    <GameLayout players={fakePlayers} dice={fakeDiceRolls} powerUps={fakePowerUps} localPlayer={fakePlayers[0]}>
       <GeneralContentWrapper>{getStageComponent(matchStage)}</GeneralContentWrapper>
     </GameLayout>
   );
