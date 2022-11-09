bcae66e09d9d51450d4fa1196ac50fb176f9fb5a
tsungai <tsungai@kryha.io>
2022-11-09 08:55:14 +0000

    Apply suggestions from code review

--- frontend/src/assets/text/general.ts
+++ frontend/src/assets/text/general.ts
@@ -16,7 +16,7 @@ export const general = {
   waitingForTheOthersToBeReady: "waiting for the others to be ready...",
   appName: "Boloney",
   gameCreated: "game created!",
-  yourGameHasBeenCreated: "your game has been successfully created! Now you can share it with your friends using the link below. Have fun!",
+  yourGameHasBeenCreated: "your match has been successfully created! Now you can share it with your friends using the link below. Have fun!",
   onlyPlayersWithThisCode: "only players with this code are able to join the match.",
   goToLobby: "go to lobby",
   ready: "ready",
