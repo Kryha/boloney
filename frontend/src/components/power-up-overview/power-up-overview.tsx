9a6f6612880e159fe5e28cb8f04b11f8510a361b
Tsungai Chipato <tsungai@kryha.io>
2022-11-09 10:02:22 +0100

    remove unsed

--- frontend/src/components/power-up-overview/power-up-overview.tsx
+++ frontend/src/components/power-up-overview/power-up-overview.tsx
@@ -5,8 +5,6 @@ import { useViewport } from "../../hooks/use-viewport";
 import { PowerUp } from "../../types";
 import { PowerUpIcon } from "../icons";
 import { PowerUpList } from "./power-up-list";
-import { useStore } from "../../store";
-import { PowerUpListOverview } from "../power-up-list-overview";
 
 interface PowerUpOverviewProps {
   powerUps?: PowerUp[];
@@ -14,11 +12,8 @@ interface PowerUpOverviewProps {
 
 export const PowerUpOverview: FC<PowerUpOverviewProps> = ({ powerUps }) => {
   const { height } = useViewport();
-  // const isModalVisible = useStore((state) => state.isModalVisible);
 
   if (!powerUps || !powerUps.length) return <></>;
-
-  // if (isModalVisible) return <PowerUpListOverview powerUps={powerUps} />;
 
   return (
     <PowerUpOverviewWrapper>
--- frontend/src/hooks/use-timer.ts
+++ frontend/src/hooks/use-timer.ts
@@ -1,20 +1,24 @@
 import { useCountdown } from "usehooks-ts";
+import { text } from "../assets";
+import { MILLISECONDS } from "../constants";
 
 const padTo2Digits = (num: number) => {
   return num.toString().padStart(2, "0");
 };
 
-export const useTimer = (seconds: number) => {
+// TODO: use ticks from nakama
+export const useTimer = (countStartSeconds: number) => {
   const [count, { startCountdown, stopCountdown, resetCountdown }] = useCountdown({
-    countStart: seconds,
-    intervalMs: 1000,
+    countStart: countStartSeconds,
+    intervalMs: MILLISECONDS,
   });
-  // 👇️ get number of full minutes
+  //  get number of full minutes
   const minutes = Math.floor(count / 60);
 
-  // 👇️ get remainder of seconds
-  const second = count % 60;
-  const time = `-${padTo2Digits(minutes)}.${padTo2Digits(second)}`;
+  //  get remainder of seconds
+  const seconds = count % 60;
+  const time = text.param.timeInMinsAndSeconds(padTo2Digits(minutes), padTo2Digits(seconds));
+
   return {
     time,
     startCountdown,
