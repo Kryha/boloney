05f1318da1421c85e86d54ac90a888985d0c408a
Marius <marius@kryha.io>
2022-11-24 12:08:28 +0000

    Merged PR 3114: Fixed dice rolling issue
    - split button from animation component
    - only rendering animation component if we have dice
    
    Related work items: #13962

--- frontend/src/components/dice-animation/rolling-dice.tsx
+++ frontend/src/components/dice-animation/rolling-dice.tsx
@@ -1,33 +1,24 @@
 import { useRef, useState, useEffect, FC } from "react";
+
 import { DiceRoll, newRoll } from "./index";
-import { text } from "../../assets";
-import { PrimaryButton } from "../buttons";
 import { rollDice } from "./roll-dice";
 import { RollerContainer } from "./styles";
-import { Die as Dice, MatchOpCode } from "../../types";
+import { Die } from "../../types";
 import { DieType } from "./types";
-import { useMatch } from "../../service/match";
 import { useStore } from "../../store";
 
 interface RollingDiceProps {
-  dice?: Dice[];
+  dice: Die[];
   dieColor: string;
+  dieType?: DieType;
 }
-export const RollingDice: FC<RollingDiceProps> = ({ dice, dieColor }) => {
-  const { broadcastPlayerReady, sendMatchState } = useMatch();
+
+export const RollingDice: FC<RollingDiceProps> = ({ dice, dieColor, dieType = DieType.D6 }) => {
   const mountRef = useRef<HTMLDivElement | null>(null);
   const [roll, setRoll] = useState<DiceRoll | undefined>();
-  const hasRolledDice = useStore((state) => state.hasRolledDice);
+
   const setIsDiceStable = useStore((state) => state.setIsDiceStable);
   const setIsDiceThrown = useStore((state) => state.setIsDiceThrown);
-
-  const handleRoll = () => {
-    if (hasRolledDice) {
-      broadcastPlayerReady();
-    } else {
-      sendMatchState(MatchOpCode.ROLL_DICE);
-    }
-  };
 
   useEffect(() => {
     let element: HTMLCanvasElement | null = null;
@@ -52,18 +43,12 @@ export const RollingDice: FC<RollingDiceProps> = ({ dice, dieColor }) => {
     };
   }, [setIsDiceStable]);
 
-  const onRoll = () => {
+  useEffect(() => {
     setIsDiceStable(false);
-    const diceRoll = newRoll(DieType.D6, dice || [], dieColor);
+    const diceRoll = newRoll(dieType, dice, dieColor);
     setIsDiceThrown(true);
     setRoll(diceRoll);
-    handleRoll();
-  };
+  }, [dice, dieColor, dieType, setIsDiceStable, setIsDiceThrown]);
 
-  // TODO: Add "ready" button after getting dice;
-  return (
-    <RollerContainer ref={mountRef}>
-      <PrimaryButton text={text.general.rollIt} onClick={() => onRoll()} />
-    </RollerContainer>
-  );
+  return <RollerContainer ref={mountRef} />;
 };
--- frontend/src/components/roll-dice/roll-dice.tsx
+++ frontend/src/components/roll-dice/roll-dice.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+
 import { avatars, text } from "../../assets";
 import { BottomButtonWrapper, Heading2 } from "../atoms";
 import { TimerHeader } from "../timer-header";
@@ -6,20 +7,31 @@ import { color } from "../../design";
 import { RollingDice } from "../dice-animation";
 import { useStore } from "../../store";
 import { ErrorView } from "../error-view";
+import { PrimaryButton } from "../buttons";
+import { useMatch } from "../../service";
+import { MatchOpCode } from "../../types";
 
 export const RollDice: FC = () => {
+  const { sendMatchState } = useMatch();
+
+  const hasRolledDice = useStore((state) => state.hasRolledDice);
   const localPlayer = useStore((state) => state.getLocalPlayer());
   const dice = useStore((state) => state.diceValue);
 
+  const handleRoll = () => {
+    sendMatchState(hasRolledDice ? MatchOpCode.PLAYER_READY : MatchOpCode.ROLL_DICE);
+  };
+
   if (!localPlayer) return <ErrorView />;
 
-  const diceColor = avatars[localPlayer.avatarId].color;
+  const dieColor = avatars[localPlayer.avatarId].color;
 
   return (
     <BottomButtonWrapper>
       <TimerHeader timeInSeconds={0} isCountDownStarted={false} title={text.powerUps.settingItUp} />
       <Heading2 customColor={color.darkGrey}>{text.param.findOutYourPips(localPlayer.username)}</Heading2>
-      <RollingDice dice={dice} dieColor={diceColor} />
+      {dice && <RollingDice dice={dice} dieColor={dieColor} />}
+      <PrimaryButton text={hasRolledDice ? text.general.imReady : text.general.rollIt} onClick={() => handleRoll()} />
     </BottomButtonWrapper>
   );
 };
