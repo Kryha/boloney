174b95deab5dc3441abdf81ee9179bc08d851f91
tsungai <tsungai@kryha.io>
2023-02-01 11:50:32 +0000

    Merged PR 3566: Power up height fix
    fix power-up card height, adjusts to min height of 640
    
    QA check that the cards still look fine in different view ports we allow up to height 640 and width 960
    
    Related work items: #16093

--- frontend/src/components/power-up-pile/styles.ts
+++ frontend/src/components/power-up-pile/styles.ts
@@ -12,7 +12,8 @@ export const PowerUpPileContainer = styled.section`
   margin-top: 30px;
   ${PowerUpCardWrapper} {
     width: clamp(171px, 17.5vw + 3px, 339px);
-    height: clamp(272px, 19.79vw + 82px, 462px);
+    height: 42.87vh;
+    min-height: 330px;
     z-index: ${zIndex.inFront};
   }
 `;
@@ -24,7 +25,8 @@ export const EmptyPowerUpCard = styled.div`
   padding: 0px;
   position: absolute;
   width: clamp(171px, 17.5vw + 3px, 339px);
-  height: clamp(272px, 19.79vw + 82px, 462px);
+  height: 42.87vh;
+  min-height: 330px;
   background: ${color.white};
   box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.2);
   border-radius: 10px;
