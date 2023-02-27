838f6846ab7e785ad97f68815347c599983297aa
tsungai <tsungai@kryha.io>
2023-02-27 11:53:26 +0000

    Merged PR 3748: Remove silly phrase from notifications
    QA
    notifications no longer contain a silly phrase.
    check the power ups, actions: boloney, exact, heal a die.
    
    Related work items: #16828

--- frontend/src/assets/text/notifications.ts
+++ frontend/src/assets/text/notifications.ts
@@ -8,7 +8,7 @@ export const notifications = {
   birdsEyeView: "BIRD'S EYE VIEW",
   vendetta: "VENDETTA",
   coup: "COUP",
-  hypnosys: "HYPNOSYS",
+  hypnosis: "HYPNOSIS",
   menageATrois: "MENAGE A TROIS",
   secondChance: "SECOND CHANCE",
   doubleUp: "DOUBLE UP",
@@ -17,20 +17,15 @@ export const notifications = {
   playerIsOutOfTheMatchTitle: (loser: string) => `${loser} is out of the match`,
   playerIsOutOfTheMatchDescription: (loser: string) =>
     `${loser} lost their last dice and goes directly to the cemetery... Let's have a minute of silence!`,
-  playerIsCallingExactOnYou: (activePlayer: string) =>
-    `${activePlayer} thinks they’re on the money with this call. Patience, young one. Your time to shine approaches.`,
-  playerIsCallingBoloneyOnYou: (activePlayer: string) =>
-    `${activePlayer} smells a bluff from you. Patience, young one. Your time to shine approaches.`,
-  idlePlayerCallingBoloney: (activeUsername: string, targetPlayer: string) =>
-    `${activeUsername} smells a bluff from ${targetPlayer}. Patience, young one. Your time to shine approaches.`,
+  playerIsCallingExactOnYou: (activePlayer: string) => `${activePlayer} thinks they’re on the money with this call.`,
+  playerIsCallingBoloneyOnYou: (activePlayer: string) => `${activePlayer} smells a bluff from you.`,
+  idlePlayerCallingBoloney: (activeUsername: string, targetPlayer: string) => `${activeUsername} smells a bluff from ${targetPlayer}.`,
   playerLeftTheMatch: (player: string) => `${player} left the match...`,
   playerIsSpreadingShockwaves: (id: PowerUpId, localPlayerName: string, callerName: string, targetName?: string | null) => {
     if (powerUpRequiresTarget(id)) {
-      return `${callerName} is feeling their power with a power-up against ${
-        localPlayerName === targetName ? "you" : targetName
-      }. Patience, young one. Your time to shine approaches.`;
+      return `${callerName} is feeling their power with a power-up against ${localPlayerName === targetName ? "you" : targetName}.`;
     } else {
-      return `${callerName} is feeling their power with a power-up. Patience, young one. Your time to shine approaches.`;
+      return `${callerName} is feeling their power with a power-up.`;
     }
   },
   playerIsUsingHealDice: (activePlayer: string) => `I'm back, baby! ${activePlayer} traded in power-ups to get back a die.`,
