19b97aadbff34eed26f5b69bc8bd85936f5cc537
Silviu Marii <silviu@kryha.io>
2023-04-12 13:47:46 +0000

    Merged PR 4029: Toggle Chat & History by default
    Chat and history are toggled by default
    
    Related work items: #18265

--- frontend/src/service/layout-config.ts
+++ frontend/src/service/layout-config.ts
@@ -7,9 +7,9 @@ export interface LayoutState {
   toggleHistory: () => void;
 }
 
-export const useLayoutStore = create<LayoutState>((set => ({
-  isChatToggled: false,
-  isHistoryToggled: false,
-  toggleChat: () => set(state => ({ isChatToggled: !state.isChatToggled })),
-  toggleHistory: () => set(state => ({ isHistoryToggled: !state.isHistoryToggled })),
-})));
+export const useLayoutStore = create<LayoutState>((set) => ({
+  isChatToggled: true,
+  isHistoryToggled: true,
+  toggleChat: () => set((state) => ({ isChatToggled: !state.isChatToggled })),
+  toggleHistory: () => set((state) => ({ isHistoryToggled: !state.isHistoryToggled })),
+}));
