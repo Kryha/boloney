7b5e3412989358c797bcdfa1dfe356db3580964b
Silviu Marii <silviu@kryha.io>
2023-05-08 08:03:35 +0000

    Merged PR 4177: Make feedback page accessible
    ###QA
    
    Feedback page should be available at /feedback .

--- frontend/src/pages/feedback/feedback.tsx
+++ frontend/src/pages/feedback/feedback.tsx
@@ -3,14 +3,14 @@ import { useNavigate } from "react-router-dom";
 
 import { text } from "../../assets";
 import { GeneralContentWrapper, Heading2, Heading6 } from "../../atoms";
-import { BaseLayout, FadeTransition, GeneralLink, Logo, MatchLayout, TopNavigation } from "../../components";
+import { BaseLayout, FadeTransition, GeneralLink, Logo } from "../../components";
 import { FEEDBACK_FORM_LINK, BLANK_TARGET_LINK } from "../../constants";
 import { PrimaryButton } from "../../molecules";
 import { routes } from "../../navigation";
 import { LinkWrapper, TitleSection } from "./styles";
 
 export const Feedback = () => {
-  return <BaseLayout leftSection={<Logo />} mainSection={<FeedbackComponent />} rightSection={<TopNavigation />} />;
+  return <BaseLayout leftSection={<Logo />} mainSection={<FeedbackComponent />} />;
 };
 
 const FeedbackComponent: FC = () => {
@@ -23,20 +23,18 @@ const FeedbackComponent: FC = () => {
 
   return (
     <FadeTransition>
-      <MatchLayout>
-        <GeneralContentWrapper>
-          <TitleSection>
-            <Heading6>{text.endOfMatch.endOfMatch}</Heading6>
-          </TitleSection>
+      <GeneralContentWrapper>
+        <TitleSection>
+          <Heading6>{text.endOfMatch.endOfMatch}</Heading6>
+        </TitleSection>
 
-          <Heading2>{text.feedback.didYouLoveit}</Heading2>
-          <Heading2>{text.feedback.helpUsImproveBoloney}</Heading2>
-          <LinkWrapper>
-            <GeneralLink linkText={text.feedback.goToHomePage} link={routes.home} isWebsite isSelfTarget />
-            <PrimaryButton primaryText={text.feedback.helpUsImprove} onClick={() => handleFeedbackFormClick()} />
-          </LinkWrapper>
-        </GeneralContentWrapper>
-      </MatchLayout>
+        <Heading2>{text.feedback.didYouLoveit}</Heading2>
+        <Heading2>{text.feedback.helpUsImproveBoloney}</Heading2>
+        <LinkWrapper>
+          <GeneralLink linkText={text.feedback.goToHomePage} link={routes.home} isWebsite isSelfTarget />
+          <PrimaryButton primaryText={text.feedback.helpUsImprove} onClick={() => handleFeedbackFormClick()} />
+        </LinkWrapper>
+      </GeneralContentWrapper>
     </FadeTransition>
   );
 };
